a85bfc744f0cd887c7cde5700c2a85739f2f1317
ho1112 <carrien1112@gmail.com>
2024-08-03 23:02:21 +0900

    refactor: postCard対応

--- src/components/post_list/PostListPage.tsx
+++ src/components/post_list/PostListPage.tsx
@@ -23,7 +23,7 @@ const PostListPage = async ({ language, category }: PostListProps) => {
       <section>
         <ul className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:gap-12'>
           {postList.map((post) => (
-            <PostCard key={post.url + post.date} post={post} />
+            <PostCard key={post.url + post.date} language={language} post={post} />
           ))}
         </ul>
       </section>
