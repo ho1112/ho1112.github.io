1b74e0b762133937855a56285a37198c3f14899d
ho1112 <carrien1112@gmail.com>
2024-08-03 22:50:56 +0900

    refactor: Headerのlanguageをparams式に変更

--- src/layouts/theme/Language.tsx
+++ src/layouts/theme/Language.tsx
@@ -1,74 +1,39 @@
-'use client';
-
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
-import i18n from "@/i18n";
+import { useRouter, usePathname } from 'next/navigation';
+import Cookies from 'js-cookie';
 
 export const Language = () => {
-  const [mounted, setMounted] = useState(false);
-  const [currentLanguage, setCurrentLanguage] = useState({
-    src: '/icon/' + (i18n.language === 'ja' ? 'japan' : 'korea') + '-flag.svg',
-    alt: i18n.language === 'ja' ? 'Japanese' : 'Korean',
-    code: i18n.language
-  });
-  const [subLanguage, setSubLanguage] = useState({
-    src: '/icon/' + (i18n.language === 'ja' ? 'korea' : 'japan') + '-flag.svg',
-    alt: i18n.language === 'ja' ? 'Korean' : 'Japanese',
-    code: i18n.language === 'ja' ? 'ko' : 'ja'
-  });
+  const router = useRouter()
+  const pathname = usePathname()
+  const currentLanguage = pathname.startsWith('/blog/ko') ? 'ko' : 'ja';
+  const newLanguage = currentLanguage === 'ko' ? 'ja' : 'ko';
 
   const switchLang = () => {
-    const newLang = i18n.language === 'ja' ? 'ko' : 'ja';
-    localStorage.setItem('i18nextLng', newLang)
-    changeLanguage(newLang)
+    const newPath = pathname.replace(`/${currentLanguage}`, `/${newLanguage}`);
+    Cookies.set('i18next', newLanguage);
+    router.push(newPath);
   }
 
-  const changeLanguage = (newLang: string) => {
-    i18n.changeLanguage(newLang, () => {
-      setCurrentLanguage({
-        src: '/icon/' + (newLang === 'ja' ? 'japan' : 'korea') + '-flag.svg',
-        alt: newLang === 'ja' ? 'Japanese' : 'Korean',
-        code: newLang,
-      });
-      setSubLanguage({
-        src: '/icon/' + (newLang === 'ja' ? 'korea' : 'japan') + '-flag.svg',
-        alt: newLang === 'ja' ? 'Korean' : 'Japanese',
-        code: newLang === 'ja' ? 'ko' : 'ja',
-      });
-    });
-  }
-
-  useEffect(() => {
-    setMounted(true);
-    const storedLang = localStorage.getItem('i18nextLng');
-    if (storedLang && i18n.language !== storedLang) {
-      i18n.changeLanguage(storedLang).then(() => {
-         // 상태 업데이트로 컴포넌트 강제 리렌더링
-         // state updateでコンポーネンんと強制リーレンダリング
-        changeLanguage(storedLang)
-      });
-    }
-  }, []);
-
-  if (!mounted) {
-    return null;
+  const imageAlt = {
+    currentLanguage: currentLanguage === 'ko' ? '한국어' : '日本語',
+    subLanguage: currentLanguage === 'ko' ? '日本語' : '한국어'
   }
 
   return (
     <div className='relative px-6 py-2 w-8 h-auto cursor-pointer rounded-md ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground aspect-square' onClick={switchLang}>
       <Image
       className='absolute left-2 bottom-3 z-[2]'
-      src={currentLanguage.src}
+      src={`/icon/${currentLanguage}-flag`+'.svg'}
       width={28}
       height={28}
-      alt={currentLanguage.alt}
+      alt={imageAlt.currentLanguage}
       />
       <Image
       className='absolute top-2 right-2 z-[1]'
-      src={subLanguage.src}
+      src={`/icon/${newLanguage}-flag`+'.svg'}
       width={26}
       height={26}
-      alt={subLanguage.alt}
+      alt={imageAlt.subLanguage}
       />
     </div>
   )
