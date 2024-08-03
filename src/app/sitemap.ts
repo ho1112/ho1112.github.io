7487a62b1f47f1253e2bdc3864183d9b4779dfc2
ho1112 <carrien1112@gmail.com>
2024-08-04 00:34:06 +0900

    fix: sitemap生成を修正

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,18 +1,23 @@
 import { MetadataRoute } from 'next';
 
 import { getSitemapPostList } from '@/lib/post';
-import { getLanguageFromCookie } from '@/utils/\bcookies';
+import { languages } from '@/config/constant';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const lang = getLanguageFromCookie()
+  const postLists = await Promise.all(
+    languages.map(async (language) => {
+      const postList = await getSitemapPostList(language);
+      return postList;
+    })
+  )
+  const flattenedPostLists = postLists.flat();
 
-  const postList = await getSitemapPostList(lang);
   const baseUrl = 'https://ho1112.github.io/';
   return [
     {
       url: baseUrl,
       lastModified: new Date(),
     },
-    ...postList,
+    ...flattenedPostLists,
   ];
 }
