85b5a4108728f981a2d3331a42ce570c6fcc3e44
ho1112 <carrien1112@gmail.com>
2024-08-11 18:37:34 +0900

    feat: add Hits api, hits icon

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -3,8 +3,8 @@ export const baseDomain = 'https://ho1112.github.io/'
 export const blogName = 'choco-mint blog'
 export const blogDesc = '東京の韓国フロントエンド'
 
-export const blogThumbnailURL = `${baseDomain}/blog_thumbnail.png`
+export const blogThumbnailURL = `${baseDomain}/blog_thumbnail.webp`
 
 export const languages = ['ko', 'ja']
 
-export const devHitsUrl = '/blog/test/test/testPost'
+export const devHitsUrl = '/blog/dev/hits/testPost'
