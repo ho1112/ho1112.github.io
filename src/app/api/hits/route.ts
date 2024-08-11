e89716e1c44e20df427303e20dfea9a49bcbca05
ho1112 <carrien1112@gmail.com>
2024-08-11 19:09:45 +0900

    test: del api dynamic

--- src/app/api/hits/route.ts
+++ src/app/api/hits/route.ts
@@ -1,7 +1,5 @@
 import { NextResponse } from 'next/server'
 
-export const dynamic = 'force-dynamic'
-
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const url = searchParams.get('url')
