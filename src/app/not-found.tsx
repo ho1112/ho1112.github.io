72cc82fa44ed216e8619fe5e6715a9cbad32366f
ho1112 <carrien1112@gmail.com>
2024-08-21 23:47:23 +0900

    refactor: 404page message

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,8 @@ export default function NotFound() {
   return (
     <div>
       <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
+      {/* <p>Could not find requested resource</p> */}
+      <p>ㅠ_ㅠ</p>
       <Link href="/">Return Home</Link>
     </div>
   )
