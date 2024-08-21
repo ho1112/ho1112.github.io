4bad3c5c5fe03357180b5e00fb21ec277a1b8b33
ho1112 <carrien1112@gmail.com>
2024-08-22 00:52:53 +0900

    feat: 404ページにrandom text設定、Homeボタン作成

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,12 +3,21 @@ import Link from 'next/link'
 export const dynamic = 'force-static'
 
 export default function NotFound() {
+  const messages = ['｡°(°.◜ᯅ◝°)°｡', '(˃̣̣̣̣︿˂̣̣̣̣ )', '(๑•́ ᎔ ก̀๑)', '╥﹏╥', '•᷄⌓•᷅']
+  const randomMessage = messages[Math.floor(Math.random() * messages.length)]
+
   return (
-    <div>
-      <h2>Not Found</h2>
-      {/* <p>Could not find requested resource</p> */}
-      <p>ㅠ_ㅠ</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-col items-center justify-center mt-28 text-center px-4">
+      <p className="text-[15vw] lg:text-[150px] whitespace-nowrap mb-6">
+        {randomMessage}
+      </p>
+      <h2 className="text-2xl font-semibold mb-4">Not Found</h2>
+      <Link
+        href="/"
+        className="border border-[#e4e4e4] rounded-md text-gray-800 leading-[38px] px-4"
+      >
+        Home
+      </Link>
     </div>
   )
 }
