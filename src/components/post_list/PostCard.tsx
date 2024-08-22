a5950346d555306f6bee2f3dc0f9cc08a8236bb9
ho1112 <carrien1112@gmail.com>
2024-08-23 00:41:50 +0900

    fix: postCard text表示をimageの上に載せる

--- src/components/post_list/PostCard.tsx
+++ src/components/post_list/PostCard.tsx
@@ -14,8 +14,9 @@ const PostCard = ({ language, post }: Props) => {
 
   return (
     <Link href={post.url}>
-      <li className="flex h-full flex-col gap-3 overflow-hidden rounded-md border shadow-md transition hover:shadow-xl dark:border-slate-700 dark:hover:border-white">
-        <div className="relative aspect-video w-full rounded-t-md border-b">
+      <li className="relative flex h-full flex-col gap-3 overflow-hidden rounded-md border shadow-md transition hover:shadow-xl dark:border-slate-700 dark:hover:border-white">
+        {/* image */}
+        <div className="w-full aspect-video rounded-t-md">
           <Image
             src={post.thumbnail}
             alt={`thumbnail for ${post.title}`}
@@ -27,31 +28,36 @@ const PostCard = ({ language, post }: Props) => {
             }}
           />
         </div>
-        <div className="flex flex-1 flex-col justify-between p-4 pt-1">
-          <div>
-            <div className="text-sm font-medium text-chomin lg:text-base">
-              {post.categoryPublicName}
+        {/* post info */}
+        <div className="absolute bottom-0 flex flex-col p-4 ">
+          <div className="flex flex-1 flex-col">
+            <div>
+              <div className="text-sm font-medium text-chomin lg:text-base">
+                <span className="bg-black px-2 py-1">
+                  {post.categoryPublicName}
+                </span>
+              </div>
+              <h2 className="my-1 text-lg text-white font-bold sm:text-xl md:text-lg">
+                {post.title}
+              </h2>
             </div>
-            <h2 className="mb-3 mt-1 text-lg font-bold sm:text-xl md:text-lg">
-              {post.title}
-            </h2>
-          </div>
-          <div className="flex justify-between gap-3 text-sm text-gray-500 dark:text-gray-400">
-            <div className="flex items-center gap-1">
-              <CalendarDays className="w-3.5" />
-              <span>
-                {post.year}
-                {t.year} {post.month}
-                {t.month} {post.day}
-                {t.day}
-              </span>
-            </div>
-            <div className="flex items-center gap-1">
-              <Clock3 className="w-3.5" />
-              <span>
-                {post.readingMinutes}
-                {t.min}
-              </span>
+            <div className="flex gap-3 text-sm text-white font-bold">
+              <div className="flex items-center gap-1">
+                <CalendarDays className="w-3.5" />
+                <span>
+                  {post.year}
+                  {t.year} {post.month}
+                  {t.month} {post.day}
+                  {t.day}
+                </span>
+              </div>
+              <div className="flex items-center gap-1">
+                <Clock3 className="w-3.5" />
+                <span>
+                  {post.readingMinutes}
+                  {t.min}
+                </span>
+              </div>
             </div>
           </div>
         </div>
