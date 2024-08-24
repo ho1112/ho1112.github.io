779ccca957d3e1ed7dfa80a504a62a153cfb2d7b
ho1112 <carrien1112@gmail.com>
2024-08-25 02:49:19 +0900

    feat: poster grid配置

--- src/components/post_list/PostCard.tsx
+++ src/components/post_list/PostCard.tsx
@@ -2,67 +2,84 @@ import { CalendarDays, Clock3 } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Post } from '@/config/types'
+import { cn } from '@/lib/utils'
 import { getLanguageText } from '@/utils/language'
 
 interface Props {
   language: string
   post: Post
+  index: number
 }
 
-const PostCard = ({ language, post }: Props) => {
+const PostCard = ({ language, post, index }: Props) => {
   const t = getLanguageText(language, 'postHeader')
 
   return (
-    <Link href={post.url}>
-      <li className="relative flex h-full flex-col gap-3 overflow-hidden rounded-md border shadow-md transition hover:shadow-xl dark:border-slate-700 dark:hover:border-white">
-        {/* image */}
-        <div className="w-full aspect-video rounded-t-md">
-          <Image
-            src={post.thumbnail}
-            alt={`thumbnail for ${post.title}`}
-            sizes="(max-width: 1000px) 50vw, 450px"
-            fill
-            priority
-            style={{
-              objectFit: 'cover',
-            }}
-          />
-        </div>
-        {/* post info */}
-        <div className="absolute bottom-0 flex flex-col p-4 ">
-          <div className="flex flex-1 flex-col">
-            <div>
-              <div className="text-sm font-medium text-chomin lg:text-base">
-                <span className="bg-black px-2 py-1">
-                  {post.categoryPublicName}
-                </span>
+    <div
+      className={cn('', {
+        'md:col-span-2 md:row-span-2': index === 0, // 첫 번째 카드, 왼쪽 전면
+        'md:col-start-3 md:row-start-1 md:col-span-2 md:row-span-1':
+          index === 1, // 두 번째 카드, 우상단
+        'md:col-start-3 md:row-start-2 md:col-span-1 md:row-span-1':
+          index === 2, // 세 번째 카드, 우하단 1열
+        'md:col-start-4 md:row-start-2 md:col-span-1 md:row-span-1':
+          index === 3, // 네 번째 카드, 우하단 2열
+      })}
+    >
+      <Link href={post.url}>
+        <li
+          className="relative flex h-full flex-col overflow-hidden rounded-md border shadow-md transition
+      hover:shadow-xl dark:border-slate-700 dark:hover:border-white"
+        >
+          {/* image */}
+          <div className="w-full aspect-video rounded-t-md">
+            <Image
+              src={post.thumbnail}
+              alt={`thumbnail for ${post.title}`}
+              sizes="(max-width: 1000px) 50vw, 450px"
+              fill
+              priority
+              style={{
+                objectFit: 'cover',
+              }}
+            />
+          </div>
+          {/* post info */}
+          <div className="absolute bottom-0 flex flex-col p-4 ">
+            <div className="flex flex-1 flex-col">
+              <div>
+                <div className="text-sm font-medium text-chomin lg:text-base">
+                  <span className="bg-black px-2 py-1">
+                    {post.categoryPublicName}
+                  </span>
+                </div>
+                <h2 className="my-1 text-lg text-white font-bold sm:text-xl md:text-lg">
+                  {post.title}
+                </h2>
               </div>
-              <h2 className="my-1 text-lg text-white font-bold sm:text-xl md:text-lg">
-                {post.title}
-              </h2>
-            </div>
-            <div className="flex gap-3 text-sm text-white font-bold">
-              <div className="flex items-center gap-1">
-                <CalendarDays className="w-3.5" />
-                <span>
-                  {post.year}
-                  {t.year} {post.month}
-                  {t.month} {post.day}
-                  {t.day}
-                </span>
-              </div>
-              <div className="flex items-center gap-1">
-                <Clock3 className="w-3.5" />
-                <span>
-                  {post.readingMinutes}
-                  {t.min}
-                </span>
+              <div className="flex gap-3 text-sm text-white font-bold">
+                <div className="flex items-center gap-1">
+                  <CalendarDays className="w-3.5" />
+                  <span>
+                    {post.year}
+                    {t.year} {post.month}
+                    {t.month} {post.day}
+                    {t.day}
+                  </span>
+                </div>
+                <div className="flex items-center gap-1">
+                  <Clock3 className="w-3.5" />
+                  <span>
+                    {post.readingMinutes}
+                    {t.min}
+                  </span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </li>
-    </Link>
+        </li>
+      </Link>
+    </div>
   )
 }
 
