e5bab2a1f8e952254d31ad79ce1e1a9d8e275f05
ho1112 <carrien1112@gmail.com>
2024-08-26 00:22:51 +0900

    fix: ScrollProgressBarが表示されないバグ修正

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -10,31 +10,33 @@ import { Button } from '@/components/ui/button'
 import { Language } from './theme/Language'
 
 export const Header = () => {
-  const { ref, transform } = useScrollTracker(65)
+  const { ref, transform } = useScrollTracker(60)
   const pathname = usePathname()
   const currentLanguage = pathname.startsWith('/blog/ko') ? 'ko' : 'ja'
 
   return (
-    <nav
-      style={{ transform }}
-      ref={ref}
-      className="fixed z-40 flex w-full flex-col items-center justify-center border-b bg-background shadow-sm"
-    >
-      <div className="mt-1 flex h-[64px] w-full max-w-[1200px] items-center justify-between px-4">
-        <div className="flex items-center text-lg font-medium">
-          <Link href={`/blog/${currentLanguage}/`}>mintora</Link>
+    <>
+      <nav
+        style={{ transform }}
+        ref={ref}
+        className="fixed z-40 flex w-full flex-col items-center justify-center border-b bg-background shadow-sm"
+      >
+        <div className="mt-1 flex h-[60px] w-full max-w-[1200px] items-center justify-between px-4">
+          <div className="flex items-center text-lg font-medium">
+            <Link href={`/blog/${currentLanguage}/`}>mintora</Link>
+          </div>
+          <div className="flex gap-3">
+            <ThemeSwitch />
+            <Language />
+            <Button asChild variant="ghost" size="icon">
+              <Link href="https://github.com/ho1112" target="_blank">
+                <Github className="size-[2rem]" />
+              </Link>
+            </Button>
+          </div>
         </div>
-        <div className="flex gap-3">
-          <ThemeSwitch />
-          <Language />
-          <Button asChild variant="ghost" size="icon">
-            <Link href="https://github.com/ho1112" target="_blank">
-              <Github className="size-[2rem]" />
-            </Link>
-          </Button>
-        </div>
-      </div>
+      </nav>
       <ScrollProgressBar />
-    </nav>
+    </>
   )
 }
