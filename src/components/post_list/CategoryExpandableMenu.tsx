a4240a02c42b4ea45e32cd41a4a165d583bc22ea
ho1112 <carrien1112@gmail.com>
2024-09-02 01:54:20 +0900

    fix: CategoryExpandableMenuをcss制御に変更

--- src/components/post_list/CategoryExpandableMenu.tsx
+++ src/components/post_list/CategoryExpandableMenu.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-
 interface Props {
   displayName: string
   isCurrent: boolean
@@ -15,31 +13,27 @@ export const CategoryExpandableMenu = ({
   isCurrent,
   subCategories,
 }: Props) => {
-  const [isOpen, setIsOpen] = useState(false)
-
   return (
-    <div
-      className="relative"
-      onMouseEnter={() => setIsOpen(true)}
-      onMouseLeave={() => setIsOpen(false)}
-    >
-      <button className={`py-2 px-4 ${isCurrent ? 'font-bold' : ''}`}>
+    <div className="relative group">
+      <button className={`pt-2 pb-3 px-4 ${isCurrent ? 'font-bold' : ''}`}>
         {displayName}
       </button>
-      {isOpen && (
-        <ul className="absolute z-10 mt-1 bg-white shadow-lg">
-          {subCategories.map((sub) => (
-            <li key={sub.subName} className="whitespace-nowrap">
-              <a
-                href={`/blog/${sub.language}/${sub.subName}/`}
-                className="block py-2 px-4 hover:bg-gray-100"
-              >
-                {sub.displayName}
-              </a>
-            </li>
-          ))}
-        </ul>
-      )}
+
+      <ul
+        className="absolute z-10 -mt-px bg-white shadow-lg
+          opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-opacity duration-300"
+      >
+        {subCategories.map((sub) => (
+          <li key={sub.subName} className="whitespace-nowrap">
+            <a
+              href={`/blog/${sub.language}/${sub.subName}/`}
+              className="block py-2 px-4 hover:bg-gray-100"
+            >
+              {sub.displayName}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
