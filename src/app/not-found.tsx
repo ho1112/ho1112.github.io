5c2f22ce143514830e2cfe262c6cd59e24bb65b7
ho1112 <carrien1112@gmail.com>
2025-04-06 14:25:40 +0900

    fix: force-dynamic

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 
-export const dynamic = 'force-static'
+export const dynamic = 'force-dynamic'
 
 export default function NotFound() {
   const messages = ['｡°(°.◜ᯅ◝°)°｡', '(˃̣̣̣̣︿˂̣̣̣̣ )', '(๑•́ ᎔ ก̀๑)', '╥﹏╥', '•᷄⌓•᷅']
