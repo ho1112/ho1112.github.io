012182cec43975a9dd08aa03c416b926506fbd5e
ho1112 <carrien1112@gmail.com>
2025-07-13 21:28:11 +0900

    fix: 다크, 라이트모드를 바로 토글할 수 있도록 변경, 드랍다운메뉴 사용안함

--- src/layouts/theme/Switch.tsx
+++ src/layouts/theme/Switch.tsx
@@ -1,21 +1,9 @@
 'use client'
 
-import { Dot, LucideIcon, Monitor, Moon, Sun } from 'lucide-react'
+import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 import { useEffect, useState } from 'react'
 import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
-
-interface DropdownItemProps {
-  t: string
-  label: string
-  Icon: LucideIcon
-}
 
 const ThemeSwitch = () => {
   const [mounted, setMounted] = useState(false)
@@ -30,30 +18,16 @@ const ThemeSwitch = () => {
     return null
   }
 
-  const Item = ({ t, Icon, label }: DropdownItemProps) => (
-    <DropdownMenuItem onClick={() => setTheme(t)} className="justify-between">
-      <div className="flex items-center gap-2">
-        <Icon width={14} /> {label}
-      </div>
-      {theme === t && <Dot />}
-    </DropdownMenuItem>
-  )
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon">
-          <Sun className="h-[2rem] w-[2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[2rem] w-[2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <Item t="light" label="Light" Icon={Sun} />
-        <Item t="dark" label="Dark" Icon={Moon} />
-        <Item t="system" label="System" Icon={Monitor} />
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button variant="ghost" size="icon" onClick={toggleTheme}>
+      <Sun className="h-[2rem] w-[2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[2rem] w-[2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   )
 }
 
